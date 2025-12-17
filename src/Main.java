7af30e8a2afc094b1caafa33b1940aeca9637a6f
guigas-cmd <diasnascimentoguilherme@gmail.com>
2025-12-17 19:03:45 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // CURSO //
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("curso js");
+        curso1.setDescricao("descricao curso js");
+        curso1.setCargaHoraria(4);
+
+        //MENTORIA //
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now()); // quando criar, vai ser na data atual/now
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+    }
+}
